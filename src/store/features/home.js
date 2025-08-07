8b3df127bdcfa6cfa242904ad8b012f54c7d6ac4
LAPTOP-KCF3SC5G\HUAWEI <a894074295@126.com>
2025-08-07 13:44:27 +0800

    1557_day86_Redux技术-RTK的使用-connect原理-11_(理解)Redux-RTK的extraReducers

--- src/store/features/home.js
+++ src/store/features/home.js
@@ -1,12 +1,23 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
 import axios from 'axios'
 
-export const fetchHomeMultidataAction = createAsyncThunk("fetch/homemultidata",async () => {
-  const res = await axios.get("http://123.207.32.32:8000/home/multidata")
-  // const banners = res.data.data.banner.list
-  // const recommends = res.data.data.recommend.list
+export const fetchHomeMultidataAction = createAsyncThunk(
+  "fetch/homemultidata",
+  async (extraInfo,{dispatch,getState}) => {
+    console.log(extraInfo,dispatch,getState)
 
-  return res.data
+    // 1.发送网络请求,获取数据
+    const res = await axios.get("http://123.207.32.32:8000/home/multidata")
+
+    // 2.取出数据,并且在此直接dispatch操作(可以不做)
+    // const banners = res.data.data.banner.list
+    // const recommends = res.data.data.recommend.list
+
+    // dispatch(changeBanners(banners))
+    // dispatch(changeRecommends(recommends))
+
+    // 3.返回结果,那么action状态会变成fulfilled状态
+    return res.data
 })
 
 const obj = {
